baee412cfcbbf6dc7429535c5a85440c31b4a9e0
Ehsan Khan <khanehsan05@gmail.com>
2025-12-09 19:35:40 +0200

    do task-L
    -in this task, I refined and polished the entire web
    -improved visuals and shortcomings from the previous version

---
+++ task-l/script.js
@@ -0,0 +1,22 @@
+<!--
+Author: Ehsan Khan
+Date: 2025-12-08
+refined Order Page
+-->
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
+
+// SIMPLE FADE-IN ON SCROLL
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".fade").forEach(el => observer.observe(el));
